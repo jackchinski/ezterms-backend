b0b6aad3f2396f6392199a90334b601292d09489
jackchinski <marcin.jacz@gmail.com>
2024-04-06 18:02:01 -0400

    x example

--- src/main.js
+++ src/main.js
@@ -31,4 +31,4 @@ async function main(companyName) {
 }
 
 // ENTER SERVICE NAME BELOW
-main("").then(() => console.log("All processes completed successfully!"));
+main("x").then(() => console.log("All processes completed successfully!"));
